121c5aeba502bd5c17fcef6755ac27b76e66e939
Ali Eyvazli <ali.eyvazli@kellogg.northwestern.edu>
2023-06-07 23:33:12 +0000

    done

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,7 @@
 class TransactionsController < ApplicationController
     def index
       @transactions = Transaction.all.includes(:from_user, :to_user)
+      @users = User.all
     end
   end
+  
